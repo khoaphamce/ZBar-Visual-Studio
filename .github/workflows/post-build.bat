5beff5a68105d17e42d218d5780eef2db9477cbe
Huy A. Tran <huy.a.tran@opswat.com>
2024-07-30 16:52:59 +0700

    Update copy files command in post-build.bat

--- .github/workflows/post-build.bat
+++ .github/workflows/post-build.bat
@@ -9,7 +9,6 @@ echo ## Finished setting up artifact folder ##
 
 echo ## Copy to artifact ##
 xcopy /y /d /s /e /k .\include\*.h .\ZBarVS\artifacts\include\
-xcopy /y /d /s /e /k .\include\zbar\ .\ZBarVS\artifacts\include\
 xcopy /y /d /s /e /k .\ZBarVS\x64\Release\*.dll .\ZBarVS\artifacts\bin\
 xcopy /y /d /s /e /k .\ZBarVS\x64\Release\*.lib .\ZBarVS\artifacts\lib\
 echo ## Finished copying to artifact ##
