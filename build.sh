5a77c24ab162fc91d7193ba80f9932d09a7e90cf
Huy A. Tran <huy.a.tran@opswat.com>
2024-07-30 15:51:58 +0700

    Update build.sh to build using Ubuntu, add GitHub Actions workflow to build Linux package automatically

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
-if [ -f /zbar/unistd.h ]; then
-    rm /zbar/unistd.h
-	echo "File /zbar/unistd.h has been removed."
-fi
-yum install autoconf libtool automake make autoconf-archive pkg-config gettext-devel
+rm ./zbar/unistd.h
+sudo apt update
+sudo apt install autoconf libtool automake make autoconf-archive pkg-config autopoint
 autoreconf -vfi
 ./configure --without-java --without-gtk --without-qt  --without-imagemagick  --disable-video --without-python --without-jpeg
 make
